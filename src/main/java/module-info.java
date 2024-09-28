e4526c18335d653364fbf5131a37ed67f65527f4
magiauy <68595741+magiauy@users.noreply.github.com>
2024-09-28 12:42:28 +0700

    Create

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module milktea.milktea {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens milktea.milktea to javafx.fxml;
+    exports milktea.milktea;
+}
